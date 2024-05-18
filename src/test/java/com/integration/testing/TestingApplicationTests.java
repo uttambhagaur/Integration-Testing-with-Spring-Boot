3d74785686a982cd167e6b1fa7043ce5e7155a31
Uttam Bhagaur <uttambhagaur@gmail.com>
2024-05-19 00:53:57 +0530

    project initialize

---
+++ src/test/java/com/integration/testing/TestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.integration.testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
